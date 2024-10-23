4675300ab42fb15fa47465eeb1f3235d9c994648
kangjun205 <kangjun205@gmail.com>
2024-10-23 01:51:06 +0000

    chore: Modify context data, embedding path

--- context_dense_embedding.py
+++ context_dense_embedding.py
@@ -13,11 +13,10 @@ from utils.embedding import context_embedding
 
 
 def main(arg):
-    context_path = arg.context_path
     model_path = arg.model_path
     batch_size = arg.batch_size
-    contexts_embedding_path = arg.contexts_embedding_path
 
+    context_path = "data/wikipedia_documents.json"
     with open(context_path, "r", encoding="utf-8") as f:
         contexts = json.load(f)
     contexts = {value["document_id"]: value["text"] for value in contexts.values()}
@@ -37,8 +36,6 @@ def main(arg):
     checkpoint = torch.load(f"{model_dir}/{model_path}")
     retrieval.load_state_dict(checkpoint["model_state_dict"])
 
-    # 학습된 retrieval으로 context의 임베딩을 생성,
-    # faiss 인덱스 생성 후 저장
     context_dataset = ContextDataset(
         context=list(contexts.values()),
         document_id=list(contexts.keys()),
@@ -48,26 +45,13 @@ def main(arg):
 
     contexts_emb = context_embedding(contextdataset=context_dataset, retrieval=retrieval, batch_size=batch_size)
     c_emb = contexts_emb["contexts_embedding"].detach().numpy().astype("float32")
+    contexts_embedding_path = "data/embedding/context_dense_embedding.bin"
     with open(contexts_embedding_path, "wb") as f:
         pickle.dump(c_emb, f)
 
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
-    args.add_argument(
-        "-c",
-        "--context_path",
-        default=None,
-        type=str,
-        help="directory path for contexts (default: None)",
-    )
-    args.add_argument(
-        "-ce",
-        "--contecontexts_embedding_path",
-        default=None,
-        type=str,
-        help="directory path for context embedding (default: None)",
-    )
     args.add_argument(
         "-m",
         "--model_path",
