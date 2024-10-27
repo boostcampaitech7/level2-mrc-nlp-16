d5523b60cdd6e4f509b10e0b5b98bacb1976a353
KyungJun <kangjun205@gmail.com>
2024-10-27 20:57:19 +0900

    docs: Add doc string to embedding.py

--- utils/embedding.py
+++ utils/embedding.py
@@ -3,6 +3,19 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 def context_embedding(contextdataset, retrieval, batch_size=2):
+    """
+    context에 대한 dense embedding module
+
+    Args:
+        contextdataset (Dataset): context data를 처리하는 dataset instance
+        retrieval (pl.LightningModule): dense embedding model
+        batch_size (int, optional): mini batch size. Defaults to 2.
+
+    Returns:
+        dict: input context에 대한 embedding 결과 return
+            - contexts_embedding: input context에 대한 embedding
+            - document_id: 각 context에 해당되는 ID
+    """
     contextloader = DataLoader(contextdataset, batch_size)
 
     mod = retrieval.mod_c.to("cuda")
